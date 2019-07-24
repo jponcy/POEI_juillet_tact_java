4e5c024a3813a1ff9af676168d72b0b6268cfc42
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2019-07-24 11:39:52 +0200

    Ajout corrections

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -19,12 +18,29 @@ public class HelloWorld {
 //
 //        declarationTableau();
 
+//        tableauLength();
+//        prediction();
+//        max();
+
+        System.out.println(purge("Bonjour", "jour"));
+        System.out.println(purge("J'ai horreur des espaces", " "));
+        System.out.println(purge("Immortel/invincible/nonMutable/brefLeMeilleur", "y"));
+
+        System.out.println(replace("Bonjour", "jour", "soir"));
+        System.out.println(replace("J'ai horreur des espaces", " ", "_"));
+        System.out.println(replace("Immortel/invincible/nonMutable/brefLeMeilleur", "y", "impossible"));
+    }
+
+    public static void tableauLength() {
         int tab[][] = {
                 {1, 2, 3, 4, 5},
                 {1, 2},
                 {1},
                 {}
         };
+
+        int max = 5;
+        int tab3[] = new int[max];
 
         for (int i = 0; i < tab.length; i ++) {
             System.out.println(
@@ -390,5 +406,359 @@ public class HelloWorld {
             System.out.println(mesJoueurs[joueur + MM]);
             System.out.println(mesJoueurs[joueur + MC]);
         }
+    }
+
+    /**
+     * Determination categorie avec un if (solution correcte).
+     */
+    public static void categorieEnfantV1() {
+        final Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quel âge à l'enfant ?");
+        String ageSaisie = scanner.nextLine();
+        scanner.close();
+        int age = Integer.parseInt(ageSaisie);
+        String categorie;
+
+        if (age >= 6 && age <= 7) {
+            categorie = "Poussin";
+        } else if (age >= 8 && age <= 9) {
+            categorie = "Pupille";
+        } else if (age >= 10 && age <= 11) {
+            categorie = "Minime";
+        } else if (age >= 12) {
+            categorie = "Cadet";
+        } else {
+            categorie = "Indéfinie";
+        }
+
+        System.out.println("L'enfant de " + age + " ans fait parti de la catégorie " + categorie);
+    }
+
+    /**
+     * Determination categorie avec un switch (solution a privilegier dans le cas present).
+     */
+    public static void categorieEnfantV2() {
+        final Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quel âge à l'enfant ?");
+        String ageSaisie = scanner.nextLine();
+        scanner.close();
+        int age = Integer.parseInt(ageSaisie);
+        String categorie;
+
+        if (age > 5) {
+            switch (age) {
+            case 6:
+            case 7:
+                categorie = "Poussin";
+                break;
+            case 8:
+            case 9:
+                categorie = "Pupille";
+                break;
+            case 10:
+            case 11:
+                categorie = "Minime";
+                break;
+            default:
+                categorie = "Cadet";
+                break;
+            }
+        } else {
+            categorie = "Indéfinie";
+        }
+
+        System.out.println("L'enfant de " + age + " ans fait parti de la catégorie " + categorie);
+    }
+
+    /**
+     * Determination categorie avec ternaire (abus des ternaires dans le cas present => complique a lire, pour rien).
+     */
+    public static void categorieEnfantV3() {
+        final Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quel âge à l'enfant ?");
+        String ageSaisie = scanner.nextLine();
+        scanner.close();
+        int age = Integer.parseInt(ageSaisie);
+
+        System.out.println("L'enfant de " + age + " ans fait parti de la catégorie "
+                + (age <= 5
+                    ? "Indéfinie"
+                    : (age >= 6 && age <= 7
+                        ? "Poussin"
+                        : (age >= 8 && age <= 9
+                            ? "Pupille"
+                            : (age >= 10 && age <= 11
+                                ? "Minime"
+                                : "Cadet")))));
+    }
+
+    public static void prediction() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quelle heure est-il ?");
+        int heures = scanner.nextInt();
+        System.out.println("Combien de minutes ?");
+        int minutes = scanner.nextInt();
+        scanner.close();
+
+        minutes ++;
+
+        if (minutes >= 60) {
+            heures ++;
+            minutes = 0;
+
+            if (heures >= 24) {
+                heures = 0;
+            }
+        }
+
+        // System.out.println("Dans 1 minute il sera " + heures + "h" + minutes);
+        // String val = (heures < 10 ? "0" : "") + heures;
+
+        String message = String.format("Dans 1 minute il sera %02dh%02d", heures, minutes);
+        System.out.println(message);
+
+//        System.out.println(String.format("%06.2f -- %g", 10000.3049, 10.3));
+//        double i = 451515458541.78545848548485;
+//        System.out.println(String.format("%f -- %g", i, i));
+    }
+
+    public static void validationDate() {
+        int jour;
+        int mois;
+        int annee;
+        boolean valide;
+        Scanner scanner = new Scanner(System.in);
+
+        // Saisies
+        System.out.print("Saisir jour : ");
+        jour = scanner.nextInt();
+        System.out.print("Saisir mois : ");
+        mois = scanner.nextInt();
+        System.out.print("Saisir année : ");
+        annee = scanner.nextInt();
+
+        // Verifications.
+        if (jour < 1 || mois < 1 || mois > 12) {
+            valide = false;
+        } else {
+            if ((mois == 1 || mois == 3 || mois == 5 || mois == 7 || mois == 8 || mois == 10 || mois == 12)
+                    && jour <= 31) {
+                valide = true;
+            } else if ((mois == 4 || mois == 6 || mois == 9 || mois == 11) && jour <= 30) {
+                valide = true;
+//            } else if (mois == 2 && annee % 4 == 0 && (!(annee % 100 == 0) || annee % 400 == 0) && jour <= 29) {
+//                valide = true;
+//            } else {
+//                valide = mois == 2 && jour <= 28;
+//            }
+            // OU
+            } else if (mois == 2) {
+//                if (annee % 4 == 0 && (!(annee % 100 == 0) || annee % 400 == 0) && jour <= 29) {
+//                    valide = true;
+//                } else if (jour <= 28) {
+//                    valide = true;
+//                } else {
+//                    valide = false;
+//                }
+                // OU.
+                valide = (annee % 4 == 0 && (!(annee % 100 == 0) || annee % 400 == 0) && jour <= 29) || jour <= 28;
+            }
+        }
+
+//    // OU "simplement"
+//    valide <- jour >= 1 ET mois >= 1 ET mois <= 12 ET (
+//        ((mois = 1 OU mois = 3 OU mois = 5 OU mois = 7 OU mois = 8 OU mois = 10 OU mois = 12) ET jour <= 31) OU
+//        ((mois = 4 OU mois = 6 OU mois = 9 OU mois = 11) ET jour <= 30) OU
+//        (mois = 2 ET annee % 4 = 0 ET (NON annee % 100 = 0 OU annee % 400 = 0)))
+//
+//    // Affichage
+//    SI valide ALORS
+//        Ecrire("Valide")
+//    SINON
+//        Ecrire("Invalide")
+//    FINSI
+//FIN
+    }
+
+    public static void saisieChiffreBorne() {
+        Scanner scanner = new Scanner(System.in);
+        int nb;
+
+        do {
+            System.out.print("Saisir un nombre entre 1 et 3 : ");
+            nb = Integer.parseInt(scanner.nextLine());
+        } while (nb < 1 || nb > 3);
+
+        scanner.close();
+    }
+
+    public static void afficheMult(int valeur) {
+        for (int i = 0; i <= 10; i ++) {
+            System.out.println(String.format("%d x %d = %d", valeur, i, valeur * i));
+        }
+    }
+
+    public static void afficheMult() {
+        System.out.println("Quel chiffre vous intéresse ?");
+        Scanner scanner = new Scanner(System.in);
+
+        afficheMult(scanner.nextInt());
+
+        scanner.close();
+    }
+
+    public static void afficheTablesDeMultiplications() {
+        for (int i = 0; i <= 10; i ++) {
+            afficheMult(i);
+            System.out.println();
+        }
+    }
+
+    public static void max() {
+        Scanner scanner = new Scanner(System.in);
+//        Integer max = null;
+        long max = Long.MIN_VALUE;
+        long saisie;
+
+        for (int i = 0; i < 20; i ++) {
+            System.out.println(String.format("Saisir le chiffre n°%d", i + 1));
+            saisie = scanner.nextLong();
+
+            if (saisie > max) {
+                max = saisie;
+            }
+        }
+
+        scanner.close();
+
+        System.out.println(String.format("La plus grande valeur saisie est %d", max));
+    }
+
+    public static Integer maxGen(int ...valeurs) {
+        Integer resultat = null;
+        int max;
+//        max(new int[] {1, 2, 3});
+//        max(1, 2, 3);
+
+        if (valeurs.length > 0) {
+            max = valeurs[0];
+
+            for (int i = 1; i < valeurs.length; i ++) {
+                if (valeurs[i] > max) {
+                    max = valeurs[i];
+                }
+            }
+        }
+
+        return resultat;
+    }
+
+    public static void sommeVecteurs() {
+        int tab1[] = {1,  2, 3, 4};
+        int tab2[] = {4, 16, 3, 9};
+        int tab3[] = new int[tab1.length];
+
+        for (int i = 0; i < tab1.length; i ++) {
+            tab3[i] = tab1[i] + tab2[i];
+        }
+    }
+
+    public static void schtroumpf() {
+        short tab1[] = {1,  2, 3, 4};
+        short tab2[] = {4, 16, 3, 9};
+        long schtroumpf = 0;
+
+//        for (int i = 0; i < tab1.length; i ++) {
+//            for (int j = 0; j < tab2.length; j ++) {
+//                schtroumpf += tab1[i] * tab2[j];
+        for (short val1 : tab1) {
+            for (short val2 : tab2) {
+                schtroumpf += val1 * val2;
+            }
+        }
+
+        System.out.println("Grand schtroumpf vaut : " + schtroumpf);
+    }
+
+    public static void caesarConcat() {
+        String alphabet;
+        String messageOriginal;
+        String messageTransforme = "";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quel sera l'alphabet à utiliser");
+        alphabet = scanner.nextLine();
+        System.out.println("Quel est le message ?");
+        messageOriginal = scanner.nextLine();
+
+        scanner.close();
+
+        for (char lettre : messageOriginal.toCharArray()) {
+            messageTransforme += alphabet.charAt(lettre - 'a');
+        }
+
+        System.out.println(String.format("Le message a envoyer est : %s", messageTransforme));
+    }
+
+    public static void caesarStringBuilder() {
+        String alphabet;
+        String messageOriginal;
+        StringBuilder sb = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quel sera l'alphabet à utiliser");
+        alphabet = scanner.nextLine();
+        System.out.println("Quel est le message ?");
+        messageOriginal = scanner.nextLine();
+
+        scanner.close();
+
+        for (char lettre : messageOriginal.toCharArray()) {
+            sb.append(alphabet.charAt(lettre - 'a'));
+        }
+
+        System.out.println(String.format("Le message a envoyer est : %s", sb));
+    }
+
+    public static String replaceSimple(String origine, String recherche, String remplacement) {
+        return origine.replaceAll(recherche, remplacement);
+    }
+    public static String purgeSimple(String origine, String recherche) {
+        return origine.replaceAll(recherche, "");
+    }
+
+    /**
+     * La fonction replaceAll présente dans Java sera plus performante.
+     */
+    public static String replace(String origine, String recherche, String remplacement) {
+        // ALGO   => Java
+        // Trouve => indexOf()
+        // Mid    => substring
+
+        StringBuilder sb = new StringBuilder();
+        int pos;
+
+        while ((pos = origine.indexOf(recherche)) != -1) {
+            if (pos > 0) {
+                sb.append(origine.substring(0, pos));
+            }
+
+            sb.append(remplacement);
+            origine = origine.substring(pos + recherche.length());
+        }
+
+//        if (!origine.equals("")) {
+        sb.append(origine);
+//        }
+
+        return sb.toString();
+    }
+
+    public static String purge(String origine, String recherche) {
+        return replace(origine, recherche, "");
     }
 }
