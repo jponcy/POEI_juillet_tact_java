c3b4bc24c1407d5c92cea2dbff1ca537aae9b959
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2019-07-25 16:05:03 +0200

    rpg - correction q1

---
+++ src/com/tactfactory/rpg/PlaybleCharacter.java
@@ -0,0 +1,15 @@
+package com.tactfactory.rpg;
+
+public class PlaybleCharacter {
+    /** The name of character. */
+    private String name;
+
+    /** The points of action. */
+    private int pa;
+
+    /** The points of move. */
+    private int pm;
+
+    /** The health points. */
+    private int hp;
+}
